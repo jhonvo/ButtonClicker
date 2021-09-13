7b8edc2b037d0b6e0814f02ce4f15efef8e1c188
jhonvo <jhon26victor@gmail.com>
2021-09-13 18:27:04 -0400

    Adjustmetns on the JS instructions

--- js/index.js
+++ js/index.js
@@ -1,8 +1,16 @@
 function changetext(element){
-  element.innerText = "Logout";
+  if (element.innerText == "Login"){
+    element.innerText = "Logout";
+  }
+  else {
+    element.innerText = "Login"
+  }
 }
 
 function remove(element){
     element.remove();
 }
 
+function message(){
+  alert("Ninga was liked!");
+}
